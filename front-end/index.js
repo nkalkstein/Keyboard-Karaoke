4ae6967542e3f32d883495e84714cdf9a894ce61
nkalkstein <nkalkstein@gmail.com>
2018-05-08 11:11:50 -0400

    removed punctuation from seed lyrics

--- front-end/index.js
+++ front-end/index.js
@@ -22,11 +22,11 @@ document.addEventListener("DOMContentLoaded", function(){
 
         // build the words with span elements around the letters
         for (let i = 0; i < lyricStore[n].content.length; i++) {
-    			const span = document.createElement("span");
-    			span.classList.add("span");
-    			span.innerHTML = lyricStore[n].content[i];
-    			words.appendChild(span);
-  		   }
+          const span = document.createElement("span");
+          span.classList.add("span");
+          span.innerHTML = lyricStore[n].content[i];
+          words.appendChild(span);
+         }
 
          document.removeEventListener("keydown", typing, false);
          lyricContainer.innerHTML = words.innerHTML
@@ -43,24 +43,47 @@ document.addEventListener("DOMContentLoaded", function(){
 
   function typing(event) {
     const spans = document.querySelectorAll('.span');
-		const typed = String.fromCharCode(event.which);
+    const typed = String.fromCharCode(event.which);
 
-		for (let i = 0; i < spans.length; i++) {
-			if (spans[i].innerText.toLowerCase() === typed.toLowerCase()) { // if typed letter is the one from the word
-				if (spans[i].classList.contains("bg")) { // if it already has class with the bg color then check the next one
-					continue;
-				} else if (spans[i].classList.contains("bg") === false && spans[i-1] === undefined || spans[i-1].classList.contains("bg") !== false ) {
+    for (let i = 0; i < spans.length; i++) {
+      if (spans[i].innerText.toLowerCase() === typed.toLowerCase()) { // if typed letter is the one from the word
+        if (spans[i].classList.contains("bg")) { // if it already has class with the bg color then check the next one
+          continue;
+        } else if (spans[i].classList.contains("bg") === false && spans[i-1] === undefined || spans[i-1].classList.contains("bg") !== false ) {
           // if it doesnt have class, if it is not first letter or if the letter before it doesnt have class
           // (this is done to avoid marking the letters who are not in order for being checked,
           // for example if you have two "A"s so to avoid marking both of them)
-					spans[i].classList.add("bg");
-					break;
-				}
-			}
-		}
+          spans[i].classList.add("bg");
+          break;
+        }
+      }
+    }
   }
 
   Song.getSongs()
   Lyric.getLyrics()
+  strikes()
 
 })
+
+
+
+
+
+function strikes(){
+      let strikes= 0;
+      strikesDiv = document.createElement("DIV")
+      strikesDiv.id = "strikes"
+      strikesDiv.innerHTML = `<h3>Ten Strikes and You are Out</h3>
+                                <p> ${strikes} </p>`
+    //   if (((document.getElementById('lyric-container').innerText)[(document.getElementById('lyric-container').innerText).length].className === "bg"){
+    //             strikes++
+    // }
+    gameOver(strikes)
+
+    function gameOver(strikes){
+      if (strikes === 10){
+        alert("Strike 10! YOU LOSE!  (You clearly don't know good music...)");
+      }
+    }
+        
