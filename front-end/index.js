90bf175114d4a3d0f131e8a73ac6117a392563da
Helen Liutongco <liutongcoh@gmail.com>
2018-05-08 10:48:35 -0400

    Add event listeners to lyrics

--- front-end/index.js
+++ front-end/index.js
@@ -6,7 +6,6 @@ document.addEventListener("DOMContentLoaded", function(){
 
   startSong.addEventListener('click', function(){
     startSong.innerText = ''
-    lyricContainer.addEventListener('keydown', typing, false)
     song.play()
     setTimeout(displayLyrics, 5600)
   })
@@ -17,14 +16,24 @@ document.addEventListener("DOMContentLoaded", function(){
 
     function displayLine(){
       // this is where we check the text and minus a point for failed attempts
-      // document.removeEventListener("keydown", typing, false);
 
       if(lyricStore[n]){
-        lyricContainer.innerText = lyricStore[n].content
-        // lyricContainer.addEventListener('keydown', typing, false)
-        duration = lyricStore[n].duration * 1000
-        n++
-        setTimeout(displayLine, duration)
+        const words = document.createElement('p')
+
+        // build the words with span elements around the letters
+        for (let i = 0; i < lyricStore[n].content.length; i++) {
+    			const span = document.createElement("span");
+    			span.classList.add("span");
+    			span.innerHTML = lyricStore[n].content[i];
+    			words.appendChild(span);
+  		   }
+
+         document.removeEventListener("keydown", typing, false);
+         lyricContainer.innerHTML = words.innerHTML
+         document.addEventListener('keydown', typing, false)
+         duration = lyricStore[n].duration * 1000
+         n++
+         setTimeout(displayLine, duration)
       }
     }
 
@@ -33,17 +42,18 @@ document.addEventListener("DOMContentLoaded", function(){
   }
 
   function typing(event) {
+    const spans = document.querySelectorAll('.span');
 		const typed = String.fromCharCode(event.which);
-    const lyricText = lyricContainer.innerText
-		for (let i = 0; i < lyricsLength.length; i++) {
-			if (lyricText[i].innerHTML === typed) { // if typed letter is the one from the word
-				if (lyricText[i].classList.contains("bg")) { // if it already has class with the bg color then check the next one
+
+		for (let i = 0; i < spans.length; i++) {
+			if (spans[i].innerText.toLowerCase() === typed.toLowerCase()) { // if typed letter is the one from the word
+				if (spans[i].classList.contains("bg")) { // if it already has class with the bg color then check the next one
 					continue;
-				} else if (lyricText[i].classList.contains("bg") === false && lyricText[i-1] === undefined || lyricText[i-1].classList.contains("bg") !== false ) {
+				} else if (spans[i].classList.contains("bg") === false && spans[i-1] === undefined || spans[i-1].classList.contains("bg") !== false ) {
           // if it doesnt have class, if it is not first letter or if the letter before it doesnt have class
           // (this is done to avoid marking the letters who are not in order for being checked,
           // for example if you have two "A"s so to avoid marking both of them)
-					lyricText[i].classList.add("bg");
+					spans[i].classList.add("bg");
 					break;
 				}
 			}
